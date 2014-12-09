3a74c455377644d8e0ade904330ae0e5e3856217
Agus <agustin.pina@dev.redmintlabs.com>
2014-12-09 11:01:51 -0300

    Added support for Rails 3.2 and Greater

--- lib/spyme/model.rb
+++ lib/spyme/model.rb
@@ -1,12 +1,22 @@
 module Spyme
   module Model
     class Location
-      include ActiveModel::Model
+      #include ActiveModel::Model
+      include ActiveModel::Validations
+      include ActiveModel::Conversion
 
       attr_accessor :latitude, :longitude
 
       validates :latitude, presence: true, numericality: { greater_than: 0.0 }
       validates :longitude, presence: true, numericality: { greater_than: 0.0 }
+
+      def initialize(params={})
+        params.each do |attr, value|
+          self.public_send("#{attr}=", value)
+        end if params
+
+        super()
+      end
 
       def to_s
         "#{latitude}, #{longitude}"
--- test/dummy/config/environments/development.rb
+++ test/dummy/config/environments/development.rb
@@ -1,4 +1,4 @@
-Rails.application.configure do
+Rails.application.config do
   # Settings specified here will take precedence over those in config/application.rb.
 
   # In the development environment your application's code is reloaded on
